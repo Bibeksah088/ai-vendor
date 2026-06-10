096ba46715a79fd9fb1e7fd2cfc488f790b78ab4
Bibek Sah <194831731+Bibeksah088@users.noreply.github.com>
2026-06-10 11:32:34 +0530

    Update Firebase.js

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -1,17 +1,17 @@
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginonecart.firebaseapp.com",
-  projectId: "loginonecart",
-  storageBucket: "loginonecart.firebasestorage.app",
-  messagingSenderId: "242165258894",
-  appId: "1:242165258894:web:0155a2ced93e20073247df"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "ai-vendor-4104d.firebaseapp.com",
+  projectId: "ai-vendor-4104d",
+  storageBucket: "ai-vendor-4104d.firebasestorage.app",
+  messagingSenderId: "391109829346",
+  appId: "1:391109829346:web:41d250707032268cd934bb"
 };
+
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-const provider = new GoogleAuthProvider()
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-
-export {auth , provider}
-
+export { auth, provider };
